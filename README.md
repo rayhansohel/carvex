c4fe6b2328fbf75be13be9222cf2cbd8e272b811
RayhanSohel <arayhansohel@gmail.com>
2024-12-23 03:26:47 +0600

    Client Side First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# <img src="src/assets/carvex-logo/caevex-text-logo-light.png" alt="" width="40px">
+# CARVEX-The Road is Open, Let’s Go
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      Carvex
+    </div>
+  )
+}
+
+export default App
