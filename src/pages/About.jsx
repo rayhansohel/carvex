fb7dc7c537b5dc6e4ee5b5ddaf55aec53b4609d9
RayhanSohel <arayhansohel@gmail.com>
2025-02-12 15:46:59 +0600

    Responsive fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,39 +21,38 @@ const About = () => {
         </div>
       </div>
 
-      <div className="px-4 py-6 md:py-20">
-        {/* Our Mission */}
-        <div className="bg-base-200 p-8 rounded-3xl container mx-auto">
-          <h2 className="text-2xl font-semibold text-primary mb-4">
-            Our Mission
-          </h2>
-          <p>
-            At Carvex, we believe that every journey should be seamless,
-            exciting, and hassle-free. Our mission is to provide reliable and
-            affordable car rental services that allow you to explore the world
-            on your terms. Whether you're heading on a business trip or a
-            spontaneous adventure, we ensure a smooth and comfortable ride every
-            time.
-          </p>
+      <div>
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 px-4 py-6 md:py-20">
+          {/* Our Mission */}
+          <div className="bg-base-200 p-6 rounded-3xl">
+            <h2 className="font-antonio text-2xl md:text-3xl font-bold mb-4">
+              Our Mission
+            </h2>
+            <p className="text-primary/70">
+              At Carvex, we believe that every journey should be seamless,
+              exciting, and hassle-free. Our mission is to provide reliable and
+              affordable car rental services that allow you to explore the world
+              on your terms. Whether you're heading on a business trip or a
+              spontaneous adventure, we ensure a smooth and comfortable ride
+              every time.
+            </p>
+          </div>
+          {/* Our Fleet */}
+          <div className="bg-base-200 p-6 rounded-3xl">
+            <h2 className="font-antonio text-2xl md:text-3xl font-bold mb-4">
+              Our Fleet
+            </h2>
+            <p className="text-primary/70">
+              Our diverse fleet includes economy cars for budget-conscious
+              travelers, spacious SUVs for family trips, and premium luxury cars
+              for those who seek style and comfort. Every vehicle is
+              well-maintained, ensuring a safe and enjoyable driving experience.
+            </p>
+          </div>
         </div>
 
         {/* Why Choose Carvex? */}
-        <div className="pt-6 md:pt-20">
           <WhyChooseUs />
-        </div>
-
-        {/* Our Fleet */}
-        <div className="bg-base-200 p-8 rounded-3xl py-6 md:py-20 container mx-auto">
-          <h2 className="text-2xl font-semibold text-primary mb-4 text-center">
-            Our Fleet
-          </h2>
-          <p>
-            Our diverse fleet includes economy cars for budget-conscious
-            travelers, spacious SUVs for family trips, and premium luxury cars
-            for those who seek style and comfort. Every vehicle is
-            well-maintained, ensuring a safe and enjoyable driving experience.
-          </p>
-        </div>
       </div>
     </div>
   );
